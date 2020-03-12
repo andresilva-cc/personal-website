8abf6629458f74d9b20239931cc4006de1d3c412
André Luiz da Silva <andreluiz.97@outlook.com>
2020-03-12 17:47:03 -0300

    Add Google Analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,10 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-160504511-1'
+    }]
   ],
   /*
    ** Nuxt.js modules
