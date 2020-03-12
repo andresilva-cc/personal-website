9c6fdede1ecd705db98ecbffadb061a6ff16215f
André Luiz da Silva <andreluiz.97@outlook.com>
2020-03-12 18:52:08 -0300

    Add Favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,10 +13,18 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'André Luiz da Silva Personal Website'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' }],
+    link: [{ rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' }],
+    link: [{ rel: 'icon' , type: 'image/png' , sizes: '16x16' , href: '/favicon/favicon-16x16.png' }],
+    link: [{ rel: 'manifest' , href: '/favicon/site.webmanifest' }],
+    link: [{ rel: 'mask-icon' , href: '/favicon/safari-pinned-tab.svg', color: '#121212' }],
+    link: [{ rel: 'shortcut icon' , href: '/favicon/favicon.ico' }],
+    meta: [{ name: 'msapplication-TileColor' , content: '#121212' }],
+    meta: [{ name: 'msapplication-config' , content: '/favicon/browserconfig.xml' }],
+    meta: [{ name: 'theme-color' , content: '#121212' }]
   },
   /*
    ** Customize the progress-bar color
