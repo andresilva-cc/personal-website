8169e2cd3ab6064436096247f290fe9eb6734216
André Luiz da Silva <andreluiz.97@outlook.com>
2020-03-12 17:09:51 -0300

    Basic layout with picture, name, job and company

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6> </v-flex>
-  </v-layout>
+  <v-row justify="center" align="center" class="text-center text-md-left">
+    <v-col cols="12" md="4">
+      <v-avatar size="250">
+        <img src="/profile.jpg" />
+      </v-avatar>
+    </v-col>
+    <v-col cols="12" md="6">
+      <div class="display-2">André Luiz da Silva</div>
+      <div class="headline pt-2">CEO & Co-Founder @ Nuxstep</div>
+    </v-col>
+  </v-row>
 </template>
